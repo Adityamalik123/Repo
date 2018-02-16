3c268b0e7f988b732f174a3231513299cc2c3651
Aditya Malik <adityamalik360@gmail.com>
2018-02-16 12:50:35 +0530

    Update tui.js

--- wpa/js/tui.js
+++ wpa/js/tui.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 				    },
                                 }
                             })
-								window.close();
+	
 								}), 'https://chat.botplatform.io');
    
 		}, false); 
